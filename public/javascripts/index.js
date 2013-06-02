38d560c5ad60eb90c8dd75dc5206b90257593720
Michael Lai <themichaellai@gmail.com>
2013-06-02 13:35:06 +0800

    styling

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -37,7 +37,7 @@
   };
 
   var data = [];
-  var w = 1000, h = 700;
+  var w = 1000, h = 550;
   var x = d3.scale.linear()
     .domain([0, 23])
     .range([0, w - (w/24) - 40]);
@@ -51,40 +51,42 @@
     .append('g')
       .attr('transform', 'translate(40, 20)');
 
+  chart.selectAll('.xrule')
+    .data(x.ticks(24))
+    .enter().append('text')
+      .attr('class', 'xrule')
+      .attr('x', function(d) { return x(d) + 15; })
+      .attr('y', 0)
+      .attr('dy', -3)
+      .attr('text-anchor', 'middle')
+      .attr('font-size', 10)
+      .text(toHumanHours);
+  var days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+  chart.selectAll('.yrule')
+    .data(y.ticks(7))
+    .enter().append('text')
+      .attr('class', 'yrule')
+      .attr('x', 0)
+      .attr('y', function(d) { return y(d) + w/24 + 3; })
+      .attr('dx', -25)
+      .attr('text-anchor', 'middle')
+      .attr('font-size', 10)
+      .text(function (d) { return days[parseInt(d, 10)]; });
+
   getData('twitter', function(data) {
     var max = _.max(data, function(d) { return d.count; }).count;
     r = d3.scale.linear()
       .domain([0, max])
       .range([0, w/48]);
 
-    chart.selectAll('.xrule')
-      .data(x.ticks(24))
-      .enter().append('text')
-        .attr('class', 'xrule')
-        .attr('x', function(d) { return x(d) + 15; })
-        .attr('y', 0)
-        .attr('dy', -3)
-        .attr('text-anchor', 'middle')
-        .attr('font-size', 10)
-        .text(toHumanHours);
-    var days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
-    chart.selectAll('.yrule')
-      .data(y.ticks(7))
-      .enter().append('text')
-        .attr('class', 'yrule')
-        .attr('x', 0)
-        .attr('y', function(d) { return y(d) + w/24 + 3; })
-        .attr('dx', -25)
-        .attr('text-anchor', 'middle')
-        .attr('font-size', 10)
-        .text(function (d) { return days[parseInt(d, 10)]; });
     circ = chart.selectAll('circle')
       .data(data, function(d) { return "" + d.hour + d.day; });
 
     circ.enter().append('circle')
       .attr('cx', function(d) { return x(d.hour) + 15; })
       .attr('cy', function(d) { return y(d.day) + w/24; })
-      .attr('r', function(d) { return r(d.count); });
+      .attr('r', function(d) { return 0; })
+      .style('fill', '#444');
 
     circ.transition()
       .duration(1000)
